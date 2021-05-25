44ba35c4955555ae6947dd6f4ce47cd7aeb53f6e
TharshikaJeyakumar <74859974+TharshikaJeyakumar@users.noreply.github.com>
2021-05-25 20:58:31 +0200

    Boxene lavet færdige, mangler dog ved formular og knapper

--- styleguide/script/script1.js
+++ styleguide/script/script1.js
@@ -47,3 +47,19 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
+
+//COLLAPSIBLE BOX (Layout) - https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_collapsible //
+var coll = document.getElementsByClassName("collapsible-layout");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
