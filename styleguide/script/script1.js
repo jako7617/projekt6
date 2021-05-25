83c0d80c74a938ec626ca04c77b708028ee307fd
TharshikaJeyakumar <74859974+TharshikaJeyakumar@users.noreply.github.com>
2021-05-25 20:21:25 +0200

    forsøget at lave collapsible boks på typografi (virker) og knapper (virker ikke)

--- styleguide/script/script1.js
+++ styleguide/script/script1.js
@@ -1,7 +1,39 @@
 console.log("script.js is running..");
 
-//COLLAPSIBLE BOX - https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_collapsible //
+//COLLAPSIBLE BOX (Logo) - https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_collapsible //
 var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+//COLLAPSIBLE BOX (Typografi) - https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_collapsible //
+var coll = document.getElementsByClassName("collapsible-typografi");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+//COLLAPSIBLE BOX (Knapper1) - https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_collapsible //
+var coll = document.getElementsByClassName("collapsible-button1");
 var i;
 
 for (i = 0; i < coll.length; i++) {
