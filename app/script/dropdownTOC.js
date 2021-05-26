b14a83f3ea5bc1637e9897a1eeb748b13ee93737
Jakob Larsen <jakoblarsen10000@gmail.com>
2021-05-26 23:31:21 +0200

    added script to TOC, deleted from main script

---
+++ app/script/dropdownTOC.js
@@ -0,0 +1,36 @@
+  var checkList = document.getElementById('list1');
+  checkList.getElementsByClassName('anchor')[0].onclick = function(evt) {
+    if (checkList.classList.contains('visible'))
+      checkList.classList.remove('visible');
+    else
+      checkList.classList.add('visible');
+  }
+
+  var checkList2 = document.getElementById('list2');
+  checkList2.getElementsByClassName('anchor')[0].onclick = function(evt) {
+    if (checkList2.classList.contains('visible'))
+      checkList2.classList.remove('visible');
+    else
+      checkList2.classList.add('visible');
+  }
+  var checkList3 = document.getElementById('list3');
+  checkList3.getElementsByClassName('anchor')[0].onclick = function(evt) {
+    if (checkList3.classList.contains('visible'))
+      checkList3.classList.remove('visible');
+    else
+      checkList3.classList.add('visible');
+  }
+  var checkList4 = document.getElementById('list4');
+  checkList4.getElementsByClassName('anchor')[0].onclick = function(evt) {
+    if (checkList4.classList.contains('visible'))
+      checkList4.classList.remove('visible');
+    else
+      checkList4.classList.add('visible');
+  }
+  var checkList5 = document.getElementById('list5');
+  checkList5.getElementsByClassName('anchor')[0].onclick = function(evt) {
+    if (checkList5.classList.contains('visible'))
+      checkList5.classList.remove('visible');
+    else
+      checkList5.classList.add('visible');
+  }
--- app/script/script.js
+++ app/script/script.js
@@ -1,49 +1,3 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  function myFunction2() {
-    document.getElementById("myDropdown2").classList.toggle("show");
-  }
-  */
-
-  var checkList = document.getElementById('list1');
-  checkList.getElementsByClassName('anchor')[0].onclick = function(evt) {
-    if (checkList.classList.contains('visible'))
-      checkList.classList.remove('visible');
-    else
-      checkList.classList.add('visible');
-  }
-
-  var checkList2 = document.getElementById('list2');
-  checkList2.getElementsByClassName('anchor')[0].onclick = function(evt) {
-    if (checkList2.classList.contains('visible'))
-      checkList2.classList.remove('visible');
-    else
-      checkList2.classList.add('visible');
-  }
-  var checkList3 = document.getElementById('list3');
-  checkList3.getElementsByClassName('anchor')[0].onclick = function(evt) {
-    if (checkList3.classList.contains('visible'))
-      checkList3.classList.remove('visible');
-    else
-      checkList3.classList.add('visible');
-  }
-  var checkList4 = document.getElementById('list4');
-  checkList4.getElementsByClassName('anchor')[0].onclick = function(evt) {
-    if (checkList4.classList.contains('visible'))
-      checkList4.classList.remove('visible');
-    else
-      checkList4.classList.add('visible');
-  }
-  var checkList5 = document.getElementById('list5');
-  checkList5.getElementsByClassName('anchor')[0].onclick = function(evt) {
-    if (checkList5.classList.contains('visible'))
-      checkList5.classList.remove('visible');
-    else
-      checkList5.classList.add('visible');
-  }
   var checkList6 = document.getElementById('list6');
   checkList6.getElementsByClassName('anchor')[0].onclick = function(evt) {
     if (checkList6.classList.contains('visible'))
@@ -106,27 +60,30 @@ function openNav() {
       document.getElementById('progress').setAttribute( 'style', '')
     }
   }
+
+
   function resetImage() {
-
     document.getElementById("imgClickAndChange").src = "images/page1-crp.png"
-
+    document.getElementById("guideText").innerHTML = "<b>Start:</b><br /> By double clicking on the name of the newly created mission on the bar on the left, you will be presented with the three views that you will interact with in order to perform the test."
 }
 
-  function changeImage() {
 
-    document.getElementById("imgClickAndChange").src = "images/page2-crop.png"
-
+function changeImage() {
+    document.getElementById("imgClickAndChange").src = "images/page4-crp.png"
+    document.getElementById("guideText").innerHTML = "<b>Step 1:</b> <br />The first view called the “calibration” allows you to change the information regarding the referencing system (model, manufacturer, configuration, frequency, position, orientation and radom). <br /><br/> It also allows you to adjust the antennas position and orientation. Lastly, you are able to adjust the payload settings, if any are connected."
 }
+
 function changeImage2() {
-
-  document.getElementById("imgClickAndChange").src = "images/page3-crop.png"
-
-} function changeImage3() {
-
-  document.getElementById("imgClickAndChange").src = "images/page4-crp.png"
-
-} function changeImage4() {
-
   document.getElementById("imgClickAndChange").src = "images/page5-crop.png"
+  document.getElementById("guideText").innerHTML = "<b>Step 2:</b> <br />The second view called the “scan” allows you to create the flights of the drone that you will be using to test the antenna. Here, you will also be doing the data gathering as well as the data processing. "
+}
 
+function changeImage3() {
+  document.getElementById("imgClickAndChange").src = "images/calipage6-cropd.png"
+  document.getElementById("guideText").innerHTML = "<b>Step 3:</b> <br />In the third and last view, called the “plot”, is where you will be viewing the created data sets."
+}
+
+function changeImage4() {
+  document.getElementById("imgClickAndChange").src = "images/scanPage7-crop.png"
+  document.getElementById("guideText").innerHTML = "<b>Step 4:</b> <br />In order to proceed with the test, you must select the scan plans you wish to perform in a flight - then you export them. The software will then present you with an estimate of how much time it will take to perform the flight. If you are not satisfied with the estimate, you have the option to change the transfer speed. This is related to the speed between the different flights. The software will then prompt you to choose where you want to export the files to, therefore you must select a location.  "
 }
