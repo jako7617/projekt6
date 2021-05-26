da5e4409120f6a896e3b87bb1a9c8e5a82b19410
TharshikaJeyakumar <74859974+TharshikaJeyakumar@users.noreply.github.com>
2021-05-26 15:38:23 +0200

    Update script1.js

--- styleguide/script/script1.js
+++ styleguide/script/script1.js
@@ -39,7 +39,7 @@ var i;
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var content = this.nextElementSibling;
+    let content = this.nextElementSibling;
     if (content.style.display === "none") {
       content.style.display = "block";
     } else {
